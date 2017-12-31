1f2bf7795709e9e328b65902375da6899a2a1961
林明旺 <linmingwang@linmingwangdeMacBook.local>
2017-12-31 17:04:49 +0800

    debug image super resolution function

--- app/api/sr.py
+++ app/api/sr.py
@@ -6,9 +6,9 @@ from .treelog import loog
 
 def process_img(img_path):
     img_arr = Image.open(img_path)
-    loog(img_arr.shape, 'img_arr1')
+    loog(img_arr, 'img_arr1')
     img_arr = img_arr.astype('uint8')
-    loog(img_arr.shape, 'img_arr2')
+    loog(img_arr, 'img_arr2')
     # img_arr = Image.open(img_path).resize((288, 288))
     img_arr = np.expand_dims(np.array(img_arr), 0)
     inp, outp = get_model(img_arr)

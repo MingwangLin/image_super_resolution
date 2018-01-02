fef399b1cf73d90cfd37a5dcdf2255aea3e9e398
林明旺 <linmingwang@linmingwangdeMacBook.local>
2018-01-02 13:42:56 +0800

    adjust CNN model

--- app/api/sr.py
+++ app/api/sr.py
@@ -25,7 +25,7 @@ def process_img(img_path):
 
 def conv_block(x, filters, size, stride=(2,2), mode='same', act=True):
     x = Conv2D(filters, (size, size), strides=stride, padding=mode)(x)
-    x = InstanceNormalization(axis=1)(x)
+    x = InstanceNormalization()(x)
     return Activation('relu')(x) if act else x
 
 
@@ -38,7 +38,7 @@ def res_block(ip, nf=64):
 def up_block(x, filters, size):
     x = keras.layers.UpSampling2D()(x)
     x = Conv2D(filters, (size, size), strides=(1, 1), padding='same')(x)
-    x = InstanceNormalization(axis=1)(x)
+    x = InstanceNormalization()(x)
     return Activation('relu')(x)
 
 

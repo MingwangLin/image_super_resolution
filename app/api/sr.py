ba3a34fccda23e956fee81457e4792e3c5af5749
林明旺 <linmingwang@linmingwangdeMacBook.local>
2018-01-03 17:43:12 +0800

    test image super resolution performance of different CNN models

--- app/api/sr.py
+++ app/api/sr.py
@@ -18,7 +18,7 @@ def process_img(img_path):
     loog(inp, outp)
     model_sr = Model(inp, outp)
     path = '/home/lin/Downloads/imagenet/'
-    weights_name = 'top_model_in2_10000.h5'
+    weights_name = 'top_model_in2_15000.h5'
     # weights_name = 'top_model_in_7100_lr-4_2000.h5'
     # weights_name = 'top_model_in_5100.h5'
     # weights_name = 'top_model_one_epoch.h5'
